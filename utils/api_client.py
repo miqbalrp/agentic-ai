8b5e4035e6d32ba1bd948465ebc74e92cd635d08
Iqbal Rahmadhan <miqbalrp@gmail.com>
2025-06-28 12:08:59 +0700

    Try to add st caching

--- utils/api_client.py
+++ utils/api_client.py
@@ -3,12 +3,14 @@ import json
 import requests
 from dotenv import load_dotenv
 
+import streamlit as st
+
 load_dotenv()
 SECTORS_API_KEY = os.getenv("SECTORS_API_KEY")
 
 headers = {"Authorization": SECTORS_API_KEY}
-def retrieve_from_endpoint(url: str) -> dict:
 
+def retrieve_from_endpoint(url: str) -> dict:
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
